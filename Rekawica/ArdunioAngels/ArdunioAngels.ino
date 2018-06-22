092e8c413b3dd152c9293d66d421a24baa603401
wwolny <wwolny1@gmail.com>
2018-06-22 19:52:19 +0200

    In folder Rekawica added proccessing code and arduino code dedicated to reading and visualizing data from the gyroscope and accelerometer.

--- Rekawica/ArdunioAngels/ArdunioAngels.ino
+++ Rekawica/ArdunioAngels/ArdunioAngels.ino
@@ -1,43 +1,62 @@
-/**
- * Adruino wysyła accelX, accelY, accelZ do processingu i laserów
- * Kiedy przycisk jest wciśnięty to wysyłamy sygnał z kątem o jaki się obraca, 
- * żeby obrócić widok.
- * Kiedy puszczamy przycisk wysyłamy dalej ostatnie wartości odczytane przy wciśniętym przecisku.
- * Potrzebuje 3 zmiennych, na które nakładam poprawki ciągle sa to outRot(X/Y/Z)
- */
-
 #include<Wire.h>
 #define MPU 0x68
 #define MPU_2G (0<<4)
 #define MPU_4G (1<<4)
 #define MPU_8G (2<<4)
 #define MPU_16G (3<<4)
-#define TAX (1<<7)
-#define TAY (1<<6)
-#define TAZ (1<<5)
+
+const float VCC = 4.98;
+const float R_DIV = 47500.0;
+const float STRAIGHT_RESISTANCE = 37300.0;//ZLE OPORY!!!!!!
+const float BEND_RESISTANCE = 90000.0;//ZLE OPORY, bo dla 3,3kOhm
 
 long  accelX,   accelY,   accelZ;
 float gForceX,  gForceY,  gForceZ;
 long  gyroX,    gyroY,    gyroZ;
 float rotX,     rotY,     rotZ;
-float outRotX,  outRotY,  outRotZ; //Info o obrocie, trzeba dodać sygnał 
+float outRotX=0,  outRotY=0,  outRotZ=0; //Info o obrocie, trzeba dodać sygnał 
+int  sensorA0;
+float flexV;
+float flexR;
 
-bool readingAngel; // Sprawdza czy zeminiamy kąt
+bool readingAngle; // Sprawdza czy zmieniamy kąt
+
+const int sensorL = A0;    //first flex
+const int sensorR = A1;    // select the pin for the LED
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(230400);
   Wire.begin();
-  setupMPU();
+  pinMode(sensorL,INPUT);
+  setupMPU();/*
   outRotX = 0;
   outRotY= 0;
-  outRotZ = 0;
-  readingAngel = true;
+  outRotZ = 0;*/
+  readingAngle = false;
 }
 
 void loop(){
+  Serial.print("sensorVal = ");
+  sensorA0 = analogRead(sensorL);
+  Serial.println(sensorA0);
+  flexV = sensorA0*VCC/1023.0;
+  flexR = R_DIV * (VCC / flexV - 1.0);
+  Serial.println( map(flexR, STRAIGHT_RESISTANCE, BEND_RESISTANCE,
+                   0, 90.0));
+  if(sensorA0<700) { //fleks na A0 uruchamia sterowanie układem odniesienia
+    readingAngle = true;
+  }else {
+    readingAngle = false;
+  }/*
+  if(analogRead(sensorR)>300) { //fleks na A1 zeruje rotację układu
+    outRotX = 0.0;
+    outRotY = 0.0;
+    outRotZ = 0.0;
+  }*/
   recordAccelRegisters();
   recordGyroRegisters();
   printData();
+  //Serial.print(analogRead(sensorL)); //narzędzie debugujące
   delay(20);
 }
 
@@ -93,7 +112,7 @@ void processGyroData() {
   rotX *= 0.0174532925;
   rotY *= 0.0174532925;
   rotZ *= 0.0174532925;
-  if(readingAngel){
+  if(readingAngle){
     outRotX += rotX;
     outRotY += rotY;
     outRotZ += rotZ;
@@ -113,25 +132,16 @@ void processGyroData() {
 }
 
 void printData() {
-  Serial.print("GyroStatic (deg)");
-  Serial.print(" X=");
-  Serial.print(outRotX);
-  Serial.print(" Y=");
-  Serial.print(outRotY);
-  Serial.print(" Z=");
-  Serial.print(outRotZ);
-  Serial.print("Gyro (deg)");
-  Serial.print(" X=");
-  Serial.print(rotX);
-  Serial.print(" Y=");
-  Serial.print(rotY);
-  Serial.print(" Z=");
-  Serial.print(rotZ);
-  Serial.print(" Accel (g)");
-  Serial.print(" X=");
-  Serial.print(gForceX);
-  Serial.print(" Y=");
-  Serial.print(gForceY);
-  Serial.print(" Z=");
-  Serial.println(gForceZ);
+  Serial.print("accX= ");
+  Serial.println(accelX-603);
+  Serial.print("accY= ");
+  Serial.println(accelY+371);
+  Serial.print("accZ= ");
+  Serial.println(accelZ+2267);
+  Serial.print("RotX= ");
+  Serial.println(outRotX/5.0);
+  Serial.print("RotY= ");
+  Serial.println(outRotY/5.0);
+  Serial.print("RotZ= ");
+  Serial.println(outRotZ/5.0);
 }
