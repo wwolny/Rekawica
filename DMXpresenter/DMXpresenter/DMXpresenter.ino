2b0cfc5d598881092cbad029dbc2e1a6bef0c716
wwolny <wwolny1@gmail.com>
2018-06-22 21:38:04 +0200

    Project SetUp

---
+++ DMXpresenter/DMXpresenter/DMXpresenter.ino
@@ -0,0 +1,14 @@
+/**
+ * Programme for Arduino board that is going to take data passed by wireless
+ * connection and pass it forward to the Raspberry Pi board that will visualize it
+ * and to the DMX512 module that will present laser show.
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
